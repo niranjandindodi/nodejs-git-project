1dee5acc6f82cb11349228b971f92f88e1ab9f6a
niranjandindodi <niranjana.dr@gmail.com>
2021-06-05 22:27:21 +0530

    About page created

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-const http = require ("http");
-
+const http = require("http");
 http
-    .createServer((req,res) =>{
-        console.log(res);
-        res.write("<h1>This Is The Home Page!</h1>");
-        res.end();
-    })
-    .listen(8080);
+  .createServer((req, res) => {
+    switch (request.url) {
+      case "/":
+        response.writeHead(200, {
+          "Content-Type": "text/html",
+        });
+        response.write("<h1>Home Page!</h1>");
+        response.end();
+        break;
+
+      case "/about":
+        response.writeHead(200, {
+          "Content-Type": "text/html",
+        });
+        response.write("<h2>About Page!</h2>");
+        response.end();
+        break;
+    }
+})
+.listen(8080);
