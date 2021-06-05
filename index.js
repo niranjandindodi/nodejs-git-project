8b494a0eab668b72dfc6f7406dcf2eed186cb118
niranjandindodi <niranjana.dr@gmail.com>
2021-06-05 15:32:34 +0530

    Bug Fixed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require ("http");
 http
     .createServer((req,res) =>{
         console.log(res);
-        res.write("<h1>Home Page!</h1>");
+        res.write("<h1>This Is The Home Page!</h1>");
         res.end();
     })
     .listen(8080);
