d73a6498974e82efa6a762ff0a7c8ac51b79e4bc
niranjandindodi <niranjana.dr@gmail.com>
2021-06-05 20:22:50 +0530

    Added 404 logging

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ http
                     response.writeHead(404, {
                         "Content-Type" : "text/html",
                     });
+			console.log("404 page has been visited!");
                     response.write("<p>404 Page Not Found!</p>");
                     response.end();
                     break;
